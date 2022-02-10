a5bc23e8a3afbed16e24c8d7f9b31dfac1c19115
soathekid <sebaoaki@gmail.com>
2022-02-10 20:12:53 -0300

    how fix

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const PageVisit = require("../models/PageVisit")
 
 router.get('/', async function(req, res, next) {
 
-     const allBooks = await Book.find({price:0}).sort({releasedOrder:1}).lean()
+     const allBooks = await Book.find().sort({releasedOrder:1}).lean()
      await PageVisit.create({user_agent:req.headers['user-agent']})
      console.log("Visit Saved: ",req.headers['user-agent'])
      res.render('index', { data: { books: allBooks , messageSent: false }});
