39ea751527e59e251088f7e53b617fabb565f2ab
soathekid <sebaoaki@gmail.com>
2022-02-13 00:37:32 -0300

    contador de visitas y saco el addthis no funciona

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const PageVisit = require("../models/PageVisit")
 router.get('/', async function(req, res, next) {
 
      const allBooks = await Book.find().sort({releasedOrder:1}).lean()
-    // await PageVisit.create({user_agent:req.headers['user-agent']})
+     await PageVisit.create({user_agent:req.headers['user-agent']})
      console.log("Visit Saved: ",req.headers['user-agent'])
      res.render('index', { data: { books: allBooks , messageSent: false }});
 });
