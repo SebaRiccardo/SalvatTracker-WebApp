b5130b33eefce1ff308882e48350078b994e0a3f
Sebastian <sebaoaki@gmail.com>
2022-02-16 16:13:28 -0300

    controla si existe .env en development enviroment

--- config/index.js
+++ config/index.js
@@ -1,7 +1,6 @@
 const dotenv = require("dotenv");
 
-if (process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "development") {
-
+if (process.env.NODE_ENV === "development") {
   const envFound = dotenv.config();
   if (envFound.error) {
     // This error should crash whole process
@@ -9,12 +8,12 @@ if (process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "developme
   }
 }
 
-module.exports={
+module.exports = {
   database_connection_url: process.env.DB_URL,
-  mail:{
+  mail: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APPLICATION_PASSWORD,
-    host:process.env.MAIL_HOST,
+    host: process.env.MAIL_HOST,
   },
-  environment: process.env.NODE_ENV
-}
+  environment: process.env.NODE_ENV,
+};
