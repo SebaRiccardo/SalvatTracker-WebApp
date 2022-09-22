2acc1d565ab027ab18a13d1ca988314c27e135d3
Soathekid <sebaoaki@gmail.com>
2022-09-22 15:30:18 -0300

    ENV if fixed

--- config/index.js
+++ config/index.js
@@ -1,8 +1,11 @@
 const dotenv = require("dotenv");
 const envFound = dotenv.config();
-if (envFound.error) {
-  // This error should crash whole process
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+
+if (process.env.NODE_ENV === "DEV") {
+  if (envFound.error) {
+    // This error should crash whole process
+    throw new Error("⚠️  Couldn't find .env file  ⚠️");
+  }
 }
 
 module.exports = {
